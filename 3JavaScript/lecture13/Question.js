a283f135d0135464b7067140770c6456c9b014a7
Vishal-7d1 <vyadav276203@gmail.com>
2026-06-22 11:37:07 +0530

    n

--- 3JavaScript/lecture13/Question.js
+++ 3JavaScript/lecture13/Question.js
@@ -116,4 +116,48 @@ const q9arr=marks1.reduce((total,n)=>total+=n,0)/marks1.length;
 console.log(q9);
 console.log(q9arr);
 
-//Question 10
+
+//Question10
+
+
+const fruits =["apple","banana","orange","apple","banana","apple"];
+    
+const q10arr=fruits.reduce((occ,fruit)=>{occ[fruit]=(occ[fruit]||0)+1;
+    return occ},{});
+
+console.log(q10arr);
+
+
+// Question 11
+const students2=[
+    {name:"Vishal ",mark:[80,90,85]},
+    {name:"Nikhil ",mark:[95,92,98]},
+    {name:"Naman ",mark:[83,91,89]},
+    {name:"Devashish ",mark:[87,99,87]}
+];
+
+const q11arr=(students2.map((s)=>{
+    return{
+
+        name:s.name,
+        avg:s.marks.reduce((total,m)=>{
+            return total+m;
+
+        },0)/s.marks.length
+
+    };
+
+})) .filter(s=>s.avg>=85)
+.sort((a,b)=>b.avg-a.avg)
+.map(s=>s.name);
+
+console.log(q11arr);
+
+
+// Question 12
+
+const students2=[
+    {name:"Laptop ",category:"Electronic"},
+    {name:"Phone ",category:"Electronic"},
+    {name:"Shirt ",category:"Clothing"}
+];
